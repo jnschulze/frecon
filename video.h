db3dbf9aacc6f24a15a959251ce86ce88976634a
Stéphane Marchesin <marcheu@chromium.org>
2015-04-23 21:16:55 -0700

    Remove DSI hack
    Now that a more recent libdrm is in place we can drop this hack.
    
    BUG=none
    TEST=compiles
    
    Change-Id: I2d3cd62fa0189476d0feea7b2a93381d855525b4
    Reviewed-on: https://chromium-review.googlesource.com/267076
    Reviewed-by: David Sodman <dsodman@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- video.h
+++ video.h
@@ -9,13 +9,6 @@
 
 #include <xf86drm.h>
 #include <xf86drmMode.h>
-/*
- * XXX Remove this when we update linux-headers
- * See crbug.com/402127
- */
-#ifndef DRM_MODE_CONNECTOR_DSI
-#define DRM_MODE_CONNECTOR_DSI 16
-#endif
 
 #include "dbus.h"
 #include "edid_utils.h"
